9aad04f3b868655ac616444eceb81be02ef0aeec
jamin-peckham-sp <jamin.peckham@sailpoint.com>
2022-09-28 08:13:56 -0500

    stuff

--- web_server/server.py
+++ web_server/server.py
@@ -1,21 +1,27 @@
-
 from quart import Quart, request
 import json
 import logging
 from discord_habitica import bot
+from habitica.habitica_service import svc
 
 logger = logging.getLogger(__name__)
 app = Quart(__name__)
 
-def 
-
 @app.route("/habitica", methods=['POST'])
 async def habitica_listener():
-    webhook_json = json.loads(await request.data)
-    if "webhookType" in webhook_json:
-        if webhook_json["webhookType"] == "groupChatReceived": # TODO: Generalize this better
-            logger.debug(webhook_json)
-            await bot.handle_webhook(webhook_json)
+    """
+    Listen for Habitica Webhooks. 
+    """
+    request = json.loads(await request.data)
+    user_id = request["data"]["user"]["id"]
+    if user_id in svc.users:
+
+
+    if "webhookType" in request:
+        if request["webhookType"] == "groupChatReceived": # TODO: Generalize this better
+            logger.debug(request)
+            await bot.handle_webhook(request)
     else:
         logger.error("Malformed webhook invocation")
     return ""
+
